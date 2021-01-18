72565a1db46412de486a2d1207f27c67dc7a20d0
tom_glb <goodMorning_glb@atguigu.com>
2021-01-18 16:07:46 +0800

    添加关于

---
+++ flink/src/main/scala/WordCount.scala
@@ -0,0 +1,13 @@
+import org.apache.flink.api.scala._
+import org.apache.flink.api.scala.{AggregateDataSet, DataSet, ExecutionEnvironment}
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.api.scala._
+
+
+object WordCount {
+	def main(args: Array[String]): Unit = {
+		
+	}
+
+}
